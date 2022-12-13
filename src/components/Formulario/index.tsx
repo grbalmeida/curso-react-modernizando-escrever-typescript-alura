d588cd56a3f07e47fab3136f881942e3317b11e7
grbalmeida <g.r.almeida@live.com>
2022-12-13 20:05:17 -0300

    Conhecendo Props

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -29,7 +29,7 @@ class Formulario extends React.Component {
                     />
                 </div>
 
-                <Botao />
+                <Botao>Adicionar</Botao>
             </form>
         )
     }
