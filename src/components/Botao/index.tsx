631c977f09beb4af2b753abfeb7de71f7ebcd081
grbalmeida <g.r.almeida@live.com>
2022-12-15 00:12:00 -0300

    Adicionando uma tarefa

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import style from './Botao.module.scss';
 
-class Botao extends React.Component<{ children: any }> {
+class Botao extends React.Component<{
+    children: any,
+    type?: 'button' | 'submit' | 'reset' | undefined;
+}> {
     render() {
+        const { type = 'button' } = this.props;
+
         return (
-            <button className={style.botao}>
+            <button type={type} className={style.botao}>
                 { this.props.children }
             </button>
         )
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,24 +1,11 @@
-import { useState } from 'react';
+import { ITarefa } from '../../types/tarefa';
 import Item from './Item';
 import style from './Lista.module.scss';
 
-function Lista() {
-    const [tarefas, setTarefas] = useState([
-        {
-            tarefa: 'React',
-            tempo: '01:30:00'
-        },
-        {
-            tarefa: 'Javascript',
-            tempo: '01:00:00'
-        }
-    ]);
-
+function Lista({ tarefas }: { tarefas: ITarefa[] }) {
     return (
         <aside className={style.listaTarefas}>
-            <h2 onClick={() => {
-                setTarefas([...tarefas, { tarefa: 'Java', tempo: '01:30:00' }])
-            }}>Estudos do dia</h2>
+            <h2>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
                     <Item
